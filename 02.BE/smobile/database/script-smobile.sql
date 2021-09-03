85e3e5ac7d83b461045473d2a65a5e2b23b494db
Trung Nguyen <sangtrung.nguyen@evizi.com>
2021-09-03 23:56:31 +0700

    Add script create MONTH_TABLE

--- 02.BE/smobile/database/script-smobile.sql
+++ 02.BE/smobile/database/script-smobile.sql
@@ -439,3 +439,29 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2021-05-22 14:41:57
+
+
+--
+-- Table structure for table `MONTH_TABLE`
+--
+
+DROP TABLE IF EXISTS `MONTH_TABLE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `MONTH_TABLE` (
+                               `MONTH_NUMBER` int NOT NULL,
+                               `MONTH_NAME` varchar(45) DEFAULT NULL,
+                               PRIMARY KEY (`MONTH_NUMBER`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `MONTH_TABLE`
+--
+
+LOCK TABLES `MONTH_TABLE` WRITE;
+/*!40000 ALTER TABLE `MONTH_TABLE` DISABLE KEYS */;
+INSERT INTO `MONTH_TABLE` VALUES (1,'January'),(2,'February'),(3,'March'),(4,'April'),(5,'May'),(6,'June'),(7,'July'),(8,'August'),(9,'Steptember'),(10,'October'),(11,'November'),(12,'December');
+/*!40000 ALTER TABLE `MONTH_TABLE` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
